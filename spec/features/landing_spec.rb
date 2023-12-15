eeca4816be3ba2489cd752d93700f9c7c2b7d186
Anthea Yur <acyur6@gmail.com>
2023-12-15 10:44:26 -0600

    Successful API call

--- spec/features/landing_spec.rb
+++ spec/features/landing_spec.rb
@@ -18,11 +18,26 @@ require "rails_helper"
 RSpec.describe "Landing Page" do
   before(:each) do
     visit root_path
+    expect(page).to have_field(:nation)
+    select 'Fire Nation', from: :nation
+    click_button "Search For Members"
   end
-
+  
   describe 'Searching for Fire Nation Members' do
-    it 'can search members by affiliation' do
-      expect(page).to have_field(:nation)
+    it 'can search members by affiliation and brings user to the search path' do
+      expect(current_path).to eq(search_path)
+    end
+
+    it "when Fire Nation is selected, the search path should return the total number of Fire Nation people" do
+      expect(page).to have_content("Total People: 97")
+    end
+
+    xit "contains a list in full detail of the first 25 members of the Fire Nation" do
+
+    end
+
+    xit "for each member, returns the name, list of allies, enemies, and any affiliations" do
+
     end
   end
 end
