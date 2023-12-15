ef502eefcd8918bdd6acac48a1f8c996ba1b0101
Anthea Yur <acyur6@gmail.com>
2023-12-15 11:42:36 -0600

    Add images to each result

--- app/poros/member.rb
+++ app/poros/member.rb
@@ -1,5 +1,5 @@
 class Member
-  attr_reader :id, :name, :allies, :enemies, :affiliation
+  attr_reader :id, :name, :allies, :enemies, :affiliation, :photo_url
   
     def initialize(data)
       @id = data[:_id]
@@ -7,6 +7,7 @@ class Member
       @allies = data[:allies].empty? ? ["None"] : data[:allies]
       @enemies = data[:enemies].empty? ? ["None"] : data[:enemies]
       @affiliation = data[:affiliation]
+      @photo_url = data[:photoUrl]
     end
   
   end
